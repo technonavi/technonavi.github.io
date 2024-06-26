1ca206af408b46b3484434da4fbaed8af00c936d
technonavi <113118502+technonavi@users.noreply.github.com>
2024-06-26 15:09:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mapdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM points";
+$result = $conn->query($sql);
+
+$html = '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,6 +27,23 @@
 <body>
     <div id="map" style="height: 600px;"></div>
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script src="js/script.js"></script>
+    <script>
+        var map = L.map("map").setView([35.6895, 139.6917], 13); // 東京の座標
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+        }).addTo(map);';
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $html .= 'L.marker([' . $row["lat"] . ', ' . $row["lng"] . ']).addTo(map).bindPopup("' . $row["name"] . '");';
+    }
+}
+
+$html .= '</script>
 </body>
-</html>
+</html>';
+
+file_put_contents('index.html', $html);
+$conn->close();
+?>
