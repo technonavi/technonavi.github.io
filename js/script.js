45361b824396b7780e734ab28d2d17189a238881
technonavi <113118502+technonavi@users.noreply.github.com>
2024-06-26 14:33:46 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,3 @@ var map = L.map('map').setView([35.6895, 139.6917], 13); // 東京の座標
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-// 例: 東京の一部ポイントを追加
-var marker = L.marker([35.6895, 139.6917]).addTo(map)
-    .bindPopup('Tokyo')
-    .openPopup();
