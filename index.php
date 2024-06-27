cafe5d1008c9d9eee309bfb2b0bab8d4ff48fa9f
technonavi <113118502+technonavi@users.noreply.github.com>
2024-06-27 23:42:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-<?php
-// api/test.php
-header('Content-Type: text/plain');
-
-echo "Hello, this is a test page for PHP on Vercel!";
-?>
+// blog.php
+$posts = [
+    ['title' => 'First Post', 'content' => 'This is my first post.'],
+    ['title' => 'Second Post', 'content' => 'This is my second post.']
+];
+foreach ($posts as $post) {
+    echo "<h2>{$post['title']}</h2>";
+    echo "<p>{$post['content']}</p>";
+}
