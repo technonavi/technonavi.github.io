7c2212485cf8733f613e1a3aaa348367dad04c7f
technonavi <113118502+technonavi@users.noreply.github.com>
2024-06-27 23:56:16 +0900

    Create post2.php

---
+++ posts/post2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Second Post</title>
+</head>
+<body>
+    <h1>Second Post</h1>
+    <p>This is my second post.</p>
+    <a href="../index.php">Back to Home</a>
+</body>
+</html>
